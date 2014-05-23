3db79d8ce7aa035330d4c5df299fb44d313d9486
lucidus21 <lucidus21@gmail.com>
2014-05-23 19:14:28 +0900

    score 0.7701 with changed TermAssociationManager

--- course/java/scorer/termproject/beomjunshin/IntappScorer.java
+++ course/java/scorer/termproject/beomjunshin/IntappScorer.java
@@ -16,127 +16,103 @@ import org.galagosearch.tupleflow.Parameters;
 
 @RequiredStatistics(statistics = {"collectionLength", "documentCount"})
 public class IntappScorer extends ScoringFunctionIterator {
-    double k_1;
-    double b;
-    double inverseDocumentFrequency;
-	double documentFrequency;
-    double documentCount;
-    double avgDocumentLength;
-    double collectionLength;
-    HashMap<Integer, ArrayList<Integer>> termPositionsMap = new HashMap<Integer, ArrayList<Integer>>();
-    HashMap<Integer, Long> byteLengthMap = new HashMap<Integer, Long>();
-    HashMap<Integer, ArrayList<String>> termListMap = new HashMap<Integer, ArrayList<String>>();
-    
-    public IntappScorer(Parameters parameters, CountIterator iterator) throws IOException {
-        super(iterator);
-        
-        // here you write your scoring function needed for a query
-        
-//        = new int[extentArray.getPosition()];
-        k_1 = parameters.get("k_1", 1);
-        b = parameters.get("b", 1);
+  double documentFrequency;
+  double documentCount;
+  double avgDocumentLength;
+  double collectionLength;
+  HashMap<Integer, ArrayList<Integer>> termPositionsMap = new HashMap<Integer, ArrayList<Integer>>();
+  HashMap<Integer, Long> byteLengthMap = new HashMap<Integer, Long>();
+  HashMap<Integer, ArrayList<String>> termListMap = new HashMap<Integer, ArrayList<String>>();
 
-        documentFrequency = 0;
-        documentCount = parameters.get("documentCount", 100000);
-        collectionLength = parameters.get("collectionLength", 1000000);
-        avgDocumentLength = collectionLength / documentCount;
-//        System.out.println(((PositionIndexReader.Iterator) iterator).getRecordString());
-        
-        
-        
-        if(iterator instanceof PositionIndexReader.Iterator) {
-        	while(!iterator.isDone()) {
-        		
-        		ArrayList<String> termList = new ArrayList<String>();
-        		
-        		int document = iterator.document();
-        		ExtentArray extentArray = ((PositionIndexReader.Iterator) iterator).extents();
-        		ArrayList<Integer> termPositions = new ArrayList<Integer>();
-        		
-        		for (int i = 0; i < extentArray.getPosition(); ++i) {
-        			termPositions.add(extentArray.getBuffer()[i].begin);
-        		}
-        		termPositionsMap.put(document, termPositions);
-        		byteLengthMap.put(document, ((PositionIndexReader.Iterator) iterator).getDocumentByteLength());
-        		
-        		iterator.nextDocument();
-        		
-        	}
+  public IntappScorer(Parameters parameters, CountIterator iterator) throws IOException {
+    super(iterator);
+
+    // here you write your scoring function needed for a query
+    documentFrequency = 0;
+    documentCount = parameters.get("documentCount", 100000);
+    collectionLength = parameters.get("collectionLength", 1000000);
+    avgDocumentLength = collectionLength / documentCount;
+
+    if(iterator instanceof PositionIndexReader.Iterator) {
+      while(!iterator.isDone()) {
+
+        ArrayList<String> termList = new ArrayList<String>(); // NOTE 아직 안 쓰이고 있음
+
+        int document = iterator.document();
+        ExtentArray extentArray = ((PositionIndexReader.Iterator) iterator).extents();
+        ArrayList<Integer> termPositions = new ArrayList<Integer>();
+
+        for (int i = 0; i < extentArray.getPosition(); ++i) {
+          termPositions.add(extentArray.getBuffer()[i].begin);
         }
-        iterator.reset();
+        termPositionsMap.put(document, termPositions);
+        byteLengthMap.put(document, ((PositionIndexReader.Iterator) iterator).getDocumentByteLength());
 
-        while (!iterator.isDone()) {
-        	
-            documentFrequency += 1;
-            iterator.nextDocument();
-        }
-        
-        iterator.reset();
-        computeInverseDocumentFrequency();
+        iterator.nextDocument();
+
+      }
+    }
+    iterator.reset();
+
+    while (!iterator.isDone()) {
+
+      documentFrequency += 1;
+      iterator.nextDocument();
     }
 
-    public void computeInverseDocumentFrequency() {
-        double numerator = documentCount - documentFrequency + 0.5;
-        double denominator = documentFrequency + 0.5;
+    iterator.reset();
+  }
 
-        inverseDocumentFrequency = Math.log(numerator / denominator);
+  public double score(int document, int length) { 
+    int count = 0;
+
+    if (iterator.document() == document) {
+      count = iterator.count();
+    }
+    return scoreCount(count, length);
+  }
+
+  public double scoreCount(int count, int length) {
+    double score=0;
+    int document = iterator.document();
+    ArrayList<Integer> termPositions = termPositionsMap.get(document);
+
+    ///////////////////////////////////////////////////////////////////////
+    // POSSIBLE FEATURES
+    // documentFrequency: number of documents that contains the query term
+    // documentCount: total number of documents in the collection
+    // collectionLength: length of collection
+    // avgDocumentLength: average document length
+    // termPositions: positions in the document for the query term
+    // count: number of term occurrence in the document for the query term
+    // length: length of current document
+    // --------------------------------------
+    // here you write your scoring function needed for a document // SAMPLE CODE. replace it.
+    double termPosWeightSum = 0;
+    if(termPositions != null && count != 0) {
+      for(int i = 0; i < termPositions.size(); i++) {
+      	double value;
+      	
+      	// TRY1 / TODO : length에 따른 weight 을 어떻게 더 잘 줄까?? 맨앞과 맨끝에? 
+      	if (length < 30) {
+      		value = ((double)(length - termPositions.get(i)) / (double)length + 1.0) / 2.0; // NOTE
+      	} else if (length < 50) {
+      		value = ((double)(length - termPositions.get(i)) / (double)length + 1.0) / 1.3; // NOTE
+      	} else if (length < 200){
+      		value = ((double)(length - termPositions.get(i)) / (double)length + 1.0) / 1.0; // NOTE 
+      	} else {
+      		value = ((double)(length - termPositions.get(i)) / (double)length + 1.0) / 1.0; // NOTE 대강봤을 때, 200이상이 롱테일 뒤에 위치할수록 가중치 더 큼 
+      	}
+
+        if (value > 0) termPosWeightSum += value; // NOTE value가 0보다 작은 경우가 발생할 수 있을까?
+        else termPosWeightSum += 1.0;
+      }
     }
 
+    double numerator = termPosWeightSum * (1 + 1); // NOTE fi(문서에서 용어 i의 tf)에 위치에 따른 가중치 부과함
+    double denominator = termPosWeightSum + (1 * (1.0 - 1 + 1 * (length/avgDocumentLength))); //
+    score = Math.log((documentCount - documentFrequency + 0.5) / (documentFrequency + 0.5)) * numerator / denominator;
 
-    public double score(int document, int length) {
-        int count = 0;
-
-        if (iterator.document() == document) {
-            count = iterator.count();
-        }
-        return scoreCount(count, length);
-    }
-
-    public double scoreCount(int count, int length) {
-    	double score=0;
-    	int document = iterator.document();
-    	ArrayList<Integer> termPositions = termPositionsMap.get(document);
-    	///////////////////////////////////////////////////////////////////////
-    	// POSSIBLE FEATURES
-    	// documentFrequency: number of documents that contains the query term
-        // documentCount: total number of documents in the collection
-    	// collectionLength: length of collection
-        // avgDocumentLength: average document length
-    	// termPositions: positions in the document for the query term
-    	// count: number of term occurrence in the document for the query term
-    	// length: length of current document
-
-        //here you write your scoring function needed for a document
-    	// SAMPLE CODE. replace it.
-    	double termPosWeightSum = 0;
-    	if(termPositions != null && count != 0) {
-    		for(int i = 0; i < termPositions.size(); i++) {
-    			double value = ((double)(length - termPositions.get(i)) / (double)length + 1.0) / 2.0;
-    			if(termPositions.get(i)/length < 0.10)
-                    value += value * 0.1;
-                if(value > 0) termPosWeightSum += value;
-    			else termPosWeightSum += 1.0;
-    		}
-    	}
-    	
-//    	System.out.println(termListMap.get(document));
-    	double numerator = termPosWeightSum * (1 + 1);
-    	double denominator = termPosWeightSum + (1 * (1.0 - 1 + 1 * (length/avgDocumentLength)));
-    	score = Math.log((documentCount - documentFrequency + 0.5) / (documentFrequency + 0.5)) * numerator / denominator;
-    	//score = documentFrequency;
-
-    	// Default baseline : BM25
-//    	double numerator = count * (1 + 1);
-//    	double denominator = count + (1 * (1.0 - 1 + 1 * (length/avgDocumentLength)));
-//    	score = numerator / denominator;
-
-        double numerator2 = count * (k_1 + 1);
-        double denominator2 = count + (k_1 * (1.0 - b + b * (length/avgDocumentLength)));
-
-        double bm25 = inverseDocumentFrequency * numerator2 / denominator2;
-
-
-        return 0.1 * score + bm25 * 0.9;
-    }
+    return score;
+  }
 }
-
--- course/java/scorer/termproject/beomjunshin/QueryModifier.java
+++ course/java/scorer/termproject/beomjunshin/QueryModifier.java
@@ -1,24 +1,28 @@
 package scorer.termproject.beomjunshin;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
 import org.galagosearch.core.parse.Document;
 import org.galagosearch.core.parse.TagTokenizer;
+import org.galagosearch.core.store.SnippetGenerator; // TODO snippet 볼 수 없을까? 
+//import org.galagosearch.exercises.TermAssoDemo;
 import org.galagosearch.exercises.TermAssociationManager;
 
 public class QueryModifier {
 	final static String CLOSER = " )";
 	final static String DIRICHLET_SCORER = "#feature:class=scorer.dirichlet.DirichletScorer( ";
 	final static String BM25_SCORER = "#feature:class=scorer.bm25.BM25Iterator( ";
-	final static String INTAPP_SCORER = "#feature:class=scorer.termproject.youngilcho.IntappScorer( ";
-    final static String NEW_INTAPP_SCORER = "#feature:class=scorer.termproject.NewIntappScorer( ";
-
+	final static String INTAPP_SCORER = "#feature:class=scorer.termproject.beomjunshin.IntappScorer( ";
 
 	public static String modifyQuery(String query) {
 		TagTokenizer tokenizer = new TagTokenizer();
 		String modQuery = query;
-		StringBuffer sbuff = new StringBuffer();
+		StringBuffer sbuff = new StringBuffer();		
+		SnippetGenerator s = new SnippetGenerator(); // DEBUG How to use it? 
+
 		// first, check out that inputed query is not a complex query by checking "#" modifier in the text
 		if(query.contains("#")) {
 			return query;
@@ -28,47 +32,45 @@ public class QueryModifier {
 			Document tokenizeResult = tokenizer.tokenize(query);
 			List<String> tokens = tokenizeResult.terms;
 
+      TermAssociationManager termAssociationManager = TermAssociationManager.get();
+      termAssociationManager.init();
+      HashMap<String, Float> expandTokens = termAssociationManager.MakeAssoTermList(tokens.get(0));
 
-            TermAssociationManager termAssociationManager = TermAssociationManager.get();
-            termAssociationManager.init();
-            String[] expandTokens = termAssociationManager.MakeAssoTermList(tokens.get(0));
-
-			double originalTermMinWeight = 1.0;
-            for(int i = 0; i < tokens.size(); i++) {
-				double tokenWeight = (1.0 * (double)(tokens.size() - i + 1 + tokens.size() + 1) / ((double) (tokens.size() + 1) * 2.0));
-
-                originalTermMinWeight = Math.min(tokenWeight, originalTermMinWeight);
-
-				// change here if you want to change term score combining policy
-				sbuff.append("#scale:weight=" +
-						tokenWeight +
-						"( ");
+      // original query with weight 1
+      for (String token: tokens) {
+				sbuff.append("#scale:weight=");
+				sbuff.append( "1" );
+				sbuff.append("( ");
 				sbuff.append(INTAPP_SCORER);
-				//sbuff.append(BM25_SCORER);
-				sbuff.append(tokens.get(i));
+				sbuff.append(token);
 				sbuff.append(CLOSER);
 				sbuff.append(CLOSER);
 				sbuff.append(" ");
-			}
+      }
 
-            // 원래 입력된 term의 위치에 따른 가중치 중 가장 낮은 걸 가중치로 줌
-           for(String s : expandTokens) {
-                sbuff.append("#scale:weight=" +
-                        originalTermMinWeight * 0.4 +
-                        "( ");
-                sbuff.append(INTAPP_SCORER);
-                //sbuff.append(BM25_SCORER);
-                sbuff.append(s);
-                sbuff.append(CLOSER);
-                sbuff.append(CLOSER);
-                sbuff.append(" ");
-            }
-
+      if (expandTokens != null) {
+        // calculate whole frequency
+	      float freqDenominator = 0; 
+	      for (String expandTokenKey: expandTokens.keySet()) {
+	      	freqDenominator += expandTokens.get(expandTokenKey);
+	      }
+	      // expand tokens with weight by assoValue
+	      for (String expandTokenKey: expandTokens.keySet()) {
+					sbuff.append("#scale:weight=");
+					float expandTokenAssoValue = expandTokens.get(expandTokenKey); 
+					sbuff.append( expandTokenAssoValue/freqDenominator );
+					sbuff.append("( ");
+					sbuff.append(INTAPP_SCORER);
+					sbuff.append(expandTokenKey);
+					sbuff.append(CLOSER);
+					sbuff.append(CLOSER);
+					sbuff.append(" ");
+	      }
+      }
 			if(sbuff.length() > 0) modQuery = sbuff.toString();
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
-
+		} 
 
 		return modQuery;
 	}
