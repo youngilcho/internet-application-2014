061989721fd130e54403dbf528ae52d412b364ab
Youngil Cho <druidcho@gmail.com>
2014-06-05 16:56:13 +0900

    기타 여러 로직을 시험 해봤음.. 0.7876이 최고점임

--- course/java/scorer/termproject/QueryModifier.java
+++ course/java/scorer/termproject/QueryModifier.java
@@ -31,21 +31,41 @@ public class QueryModifier {
             termAssociationManager.init();
             HashMap<String, Float> expandTokens = termAssociationManager.MakeAssoTermList(tokens.get(0));
 
-            // original query with weight
+//            if(query.contains("-") && tokens.size() > 1 && expandTokens != null) {
+//                expandTokens.putAll(termAssociationManager.MakeAssoTermList(tokens.get(1)));
+//                ArrayList<TermAssociationManager.TermFreq> orderedTermList = TermAssociationManager.get().getSortedList2(expandTokens);
+//                expandTokens.clear();
+//                for(int i = 0; i < 10; i++) {
+//                    expandTokens.put(orderedTermList.get(i).term, orderedTermList.get(i).frequency);
+//                }
+//
+//            }
 
+
+            // 분석해보니 이건 오로지 d-mark가 dm rate 라는 걸로 함께 쓰이는 3개 document가 존재하기 때문이었음.. 따라서 문서가 바뀌면 의미가 있냐?
             if (query.toLowerCase().contains("d-mark"))
                 tokens.add("dm");
 
-            if (query.contains("-")) {
-                tokens.add(query.trim().replace("-", ""));
-
-                String[] temp = query.trim().split("-");
+            // 쿼리가 세 단어 이상이면서 중간에 of 가 없으면 두문자를 생성해서 토큰에 추가해봄(소폭 상승. eg Customer Price Index)
+            if (tokens.size() > 2 && !tokens.contains("of") && !tokens.contains("and")) {
                 String initialAcronym = "";
-                for (String x : temp) {
+                for (String x : tokens) {
                     initialAcronym += x.charAt(0);
                 }
-                //tokens.add(initialAcronym);
+                tokens.add(initialAcronym);
+            }
 
+
+            if (query.contains("-")) {
+                // 중간에 하이픈이 있는 쿼리는 하이픈만 빼고 붙여서 토큰에 추가함(점수 오름)
+                tokens.add(query.trim().replace("-", ""));
+                // d-mark가 dm rate로 쓰인 것처럼 하이픈이 있는 애들을 두문자 처리 해보았으나 점수 향상에 도움이 안되서 생략.
+//                String[] temp = query.trim().split("-");
+//                String initialAcronym = "";
+//                for (String x : temp) {
+//                    initialAcronym += x.charAt(0);
+//                }
+//                tokens.add(initialAcronym);
             }
 
             for (String token : tokens) {
